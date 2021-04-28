28f32af6846518dfa49395ac45a9e4168856fc7f
t-rav <tbharmon03@gmail.com>
2021-04-28 17:55:11 -0500

    Completed calculator functionality.

--- scripts.js
+++ scripts.js
@@ -3,13 +3,17 @@ const operatorBtns = document.querySelectorAll("[data-operator]");
 const clearBtn = document.querySelector("[data-clear]");
 const equalBtn = document.querySelector("[data-equals]");
 const decimalBtn = document.querySelector("[data-point]");
+
 const calcScreen = document.querySelector("[data-input]");
+
 let firstValue = "0";
 let secondValue = "";
-let evaluation = null;
+let evaluation = "";
 let needsReset = false;
 
 window.addEventListener("load", clearScreen())
+
+/* Click Event Listeners for each number, operator, and clear button. */
 
 numberBtns.forEach((button) =>
     button.addEventListener("click", () => printNum(button.textContent))
@@ -21,25 +25,48 @@ operatorBtns.forEach((button) =>
 
 clearBtn.addEventListener("click", () => clearScreen());
 
+equalBtn.addEventListener("click", () => evaluate());
+
+decimalBtn.addEventListener("click", () => placeDecimal());
+
+/* Print the clicked number on the display screen in succession. */
+
 function printNum(num) {
-    if (calcScreen.textContent === "0" || needsReset) resetScreen();
+    if (calcScreen.textContent === "0" || calcScreen.textContent === firstValue || needsReset === true) resetScreen()
     calcScreen.textContent += num;
 }
 
+function placeDecimal() {
+    if (needsReset) resetScreen();
+    if (calcScreen.textContent === "") calcScreen.textContent = "0";
+    if (calcScreen.textContent.includes(".")) return;
+    calcScreen.textContent += ".";
+}
+
+/* Evaluates the function based on user input. Signals to reset the screen for next user input. */
+
 function storeValues(operator) {
-    if (evaluation !== null ) evaluate();
-    firstValue = calcScreen.textContent;
+    if (firstValue === "" )
+    firstValue = parseFloat(calcScreen.textContent);
     evaluation = operator;
     needsReset = true;
 }
 
+/* Clearing/Resetting the Screen */
+
 function clearScreen() {
     calcScreen.textContent="0";
+    firstValue = "";
+    secondValue = "";
+    evaluation = "";
 }
 
 function resetScreen() {
     calcScreen.textContent="";
+    needsReset = false;
 }
+
+/* Math Functions for addition, subtraction, multiplication, and division. */
 
 function addNumbers(a, b) {
     return a + b;
@@ -54,8 +81,13 @@ function multiplyNumbers(a, b) {
 }
 
 function divideNumbers(a, b) {
+    if ( b === 0 || b === "" || b === null ){
+        alert("You cannot divide by Zero in this universe.");
+    } else
     return a / b;
 }
+
+/* Function to choose the correct math function based on user input. */
 
 function operate(operator, num1, num2) {
     if (operator === "+" ) {
@@ -68,3 +100,19 @@ function operate(operator, num1, num2) {
         return divideNumbers(num1, num2);
     }
 }
+
+function evaluate() {
+    secondValue = parseFloat(calcScreen.textContent);
+    calcScreen.textContent = roundResult(
+        operate(evaluation, firstValue, secondValue)
+        );
+    firstValue = parseFloat(calcScreen.textContent);
+    secondValue = "";
+    evaluation = "";
+    needsReset = true;
+}
+
+/* Round Result */
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
+  }
