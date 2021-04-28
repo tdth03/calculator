a8c292dfff6be85c0811f36bb996df47dba7dcb5
t-rav <tbharmon03@gmail.com>
2021-04-27 20:34:57 -0500

    Basic function started. Styling completed.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,45 @@
+const numberBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const clearBtn = document.querySelector("[data-clear]");
+const equalBtn = document.querySelector("[data-equals]");
+const decimalBtn = document.querySelector("[data-point]");
+const calcScreen = document.querySelector("[data-input]");
+let firstValue = "0";
+let secondValue = "";
+let evaluation = null;
+let needsReset = false;
 
+window.addEventListener("load", clearScreen())
 
+numberBtns.forEach((button) =>
+    button.addEventListener("click", () => printNum(button.textContent))
+);
 
+operatorBtns.forEach((button) =>
+    button.addEventListener("click", () => storeValues(button.textContent))
+);
 
+clearBtn.addEventListener("click", () => clearScreen());
+
+function printNum(num) {
+    if (calcScreen.textContent === "0" || needsReset) resetScreen();
+    calcScreen.textContent += num;
+}
+
+function storeValues(operator) {
+    if (evaluation !== null ) evaluate();
+    firstValue = calcScreen.textContent;
+    evaluation = operator;
+    needsReset = true;
+}
+
+function clearScreen() {
+    calcScreen.textContent="0";
+}
+
+function resetScreen() {
+    calcScreen.textContent="";
+}
 
 function addNumbers(a, b) {
     return a + b;
