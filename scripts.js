3d8f1166f24acf021323f1dabf1f6f00e06b70e0
t-rav <tbharmon03@gmail.com>
2021-05-05 10:05:23 -0500

    Trying to refactor keypad/click event listeners to better correpsond with each other.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const buttons = document.querySelectorAll("button");
 const numberBtns = document.querySelectorAll("[data-number]");
 const operatorBtns = document.querySelectorAll("[data-operator]");
 const clearBtn = document.querySelector("[data-clear]");
@@ -15,17 +16,19 @@ window.addEventListener("load", clearScreen())
 
 /* Click Event Listeners for each number, operator, and clear button. */
 
-numberBtns.forEach((button) =>
+/* numberBtns.forEach((button) =>
     button.addEventListener("click", () => printNum(button.textContent))
 );
 
 operatorBtns.forEach((button) =>
     button.addEventListener("click", () => storeValues(button.textContent))
-);
+); */
 
 clearBtn.addEventListener("click", () => clearScreen());
 
-equalBtn.addEventListener("click", () => evaluate());
+/* equalBtn.addEventListener("click", () => evaluate()); */
+
+buttons.forEach(button => keyboardInput(button));
 
 decimalBtn.addEventListener("click", () => placeDecimal());
 
@@ -49,27 +52,24 @@ function placeDecimal() {
 
 function storeValues(operator) {
     if (firstValue === "" || evaluation === "" ) {
-    firstValue = parseFloat(calcScreen.textContent);
+    firstValue = calcScreen.textContent;
     evaluation = operator;
-    console.log(firstValue);
-    console.log(secondValue);
-    console.log(evaluation);
     } else if (firstValue !== "") {
         evaluate();
         evaluation = operator;
     }
-    needsReset = true;
 }
 
   /*Takes keyboard/numpad input and returns same results as clicking each button. */
 
   function keyboardInput(e) {
-    if (e.key >= 0 && e.key <= 9) parseFloat(printNum(e.key));
+    if (e.key >= 0 && e.key <= 9) printNum(e.key);
     if (e.key === ".") placeDecimal();
-    if (e.key === "=") evaluate();
+    if (e.key === "=" || e.key === "Enter") evaluate();
     if (e.key === "Escape") clearScreen();
     if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
       storeValues(e.key);
+      console.log(e.key);
 }
 
 /* Clearing/Resetting the Screen */
@@ -110,30 +110,31 @@ function divideNumbers(a, b) {
 /* Function to choose the correct math function based on user input. */
 
 function operate(operator, num1, num2) {
-    if (operator === "+") {
-        return addNumbers(num1, num2);
-    } else if (operator === "-") {
-        return subtractNumbers(num1, num2);
-    } else if (operator === "*") {
-        return multiplyNumbers(num1, num2);
-    } else if (operator === "/") {
-        return divideNumbers(num1, num2);
+    num1 = Number(num1);
+    num2 = Number(num2);
+    switch (operator) {
+        case "+":
+            return addNumbers(num1, num2);
+        case "-":
+            return subtractNumbers(num1, num2);
+        case "*":
+            return multiplyNumbers(num1, num2);
+        case "/":
+            if (num2 === 0) alert("You cannot divide by Zero in this universe.");
+            else return divideNumbers(num1, num2);
     }
-}
+} 
 
 function evaluate() {
     if (evaluation === "") {
     firstValue = calcScreen.textContent;
     return firstValue;
     } else
-    secondValue = parseFloat(calcScreen.textContent);
-    console.log(firstValue);
-    console.log(secondValue);
-    console.log(evaluation);
+    secondValue = calcScreen.textContent;
     calcScreen.textContent = roundResult(
         operate(evaluation, firstValue, secondValue)
         );
-    firstValue = parseFloat(calcScreen.textContent);
+    firstValue = calcScreen.textContent;
     secondValue = "";
     evaluation = "";
     needsReset = true;
